c8fa9678f8f2e6ef54ab18e35ebf976816c4d517
harel <harel.yona@mail.huji.ac.il>
2023-05-02 14:25:33 +0300

    rache the best

--- numeric1.py
+++ numeric1.py
@@ -1,8 +1,9 @@
 import math
 import random
 import numpy
-mass = 9.109
-charge = -1.602
+import matplotlib.pyplot as plt
+mass = 9.109*10**(-31)
+charge = -1.602*10**(-19)
 tau = 10 ** -15
 electric_field = 30
 
@@ -17,18 +18,19 @@ def location_per_collision(x_initial, y_initial, vx_initial, vy_initial):
 def random_angle():
     return random.random()*2*math.pi
 
+
 def collision():
     """
 
     :return: the velocity after a collision
     """
     angle = random_angle()
-    vx = 0.0002*math.cos(angle)
+    vx = 0.0002 * math.cos(angle)
     vy = 0.0002 * math.sin(angle)
     return vx, vy
 
 
-def hundred_collisions_process(x_initial, y_initial, vx_initial, vy_initial):
+def hundred_collisions_process(x, y, vx, vy):
     """
     :param vy_initial: starting speed at y direction
     :param vx_initial: starting speed at x direction
@@ -36,17 +38,19 @@ def hundred_collisions_process(x_initial, y_initial, vx_initial, vy_initial):
     :param y_initial: start y coordinate
     :return: x,y coordinates after 100 collisions
     """
-    x, y = location_per_collision(x_initial, y_initial, vx_initial, vy_initial)
     x_coordinates = []
     y_coordinates = []
     for i in range(100):
-        vx, vy = collision()
         x, y = location_per_collision(x, y, vx, vy)
+        vx, vy = collision()
         x_coordinates.append(x)
         y_coordinates.append(y)
     x_coordinates = numpy.array(x_coordinates)
     y_coordinates = numpy.array(y_coordinates)
-    return x, y, x_coordinates, y_coordinates
+    return x_coordinates, y_coordinates
 
-ff
-print(hundred_collisions_process(0,0,0,0))
+
+x_coordinates, y_coordinates = hundred_collisions_process(0, 0, 0, 0)
+
+plt.plot(x_coordinates, y_coordinates)
+plt.show()
